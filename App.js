af3d5f413ddd36933f078406e6c9f0c435df5522
theeramate1999 <6110110217@psu.ac.th>
2020-08-22 16:31:44 +0700

    first commit

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+    const doIt = () => {
+      console.log("Hello form console")
+    }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress = {doIt}>Hello Word</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color : 'white',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
