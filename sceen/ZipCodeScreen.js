7f00afb57f22278d6f5bcef5065bed9133e36518
theeramate1999 <6110110217@psu.ac.th>
2020-08-23 02:54:27 +0700

    commit 1 เพิ่มการใส่รูปไปในช่องปุ่มกด

--- sceen/ZipCodeScreen.js
+++ sceen/ZipCodeScreen.js
@@ -1,29 +1,42 @@
 import React from 'react'
-import { FlatList, View, Text, StyleSheet } from 'react-native'
+import { FlatList, View, Text, StyleSheet, ImageBackground, Image } from 'react-native'
 import { TouchableHighlight } from 'react-native-gesture-handler'
 import WeatherScreen from './WeatherScreen'
 import { useNavigation } from '@react-navigation/native'
 
 
 const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
+    { place: 'Hatyai', code: '90110',    img:require('../hatyai.jpg') },
+    { place: 'Trang', code: '92000' ,    img:require('../trang.jpg') },
+    { place: 'Chiangmai', code: '50000', img:require('../chiangmai.jpg')},
+    { place: 'Khonkaen', code: '40000',  img:require('../khonkaen.jpg') },
+    { place: 'Bangkok', code: '10200' ,  img:require('../bangkok.jpg')},
+    { place: 'Ayutthaya', code: '13000' ,img:require('../ayutthaya.jpg')},
+    { place: 'Phuket', code: '83000',    img:require('../phuket.jpg') },
+    { place: 'Suratthani', code: '84000' ,img:require('../suratthani.jpg')},
    ]
    
-const ZipItem = ({place, code, navigation}) =>(
+  
+
+const ZipItem = ({place, code, img, navigation}) =>(
     <TouchableHighlight onPress = {() => {
         navigation.navigate('Weather', {zipCode: code})
     }}>
+        
+
         <View style = {styles.zipItem}>
-            <Text style = {styles.zipPlace}>{place}</Text>
-            <Text>{code}</Text>
+            <ImageBackground source = {img} style = {styles.backdrop}>
+                 <Text  style = {styles.zipPlace}>{place}</Text>
+                 <Text style = {styles.zipCode}>{code}</Text>
+                 
+             </ImageBackground>  
         </View>
+        
     </TouchableHighlight>
 
 )
+
+
 export default function ZipcodeScreen(){
     const navigation = useNavigation()
     return (
@@ -40,15 +53,30 @@ const styles = StyleSheet.create({
     zipItem:{
         flex: 1,
         flexDirection: 'row',
-        justifyContent : 'space-between',
+        
     },
     zipPlace: {
         flex: 1,
-        flexDirection: 'row',
+        fontSize:36,
+        color: 'white',
+        
         
     },
     zipCode : {
-        flex: 1,
-    }
-
+        flex: 1,       
+        fontSize:36,
+        color: 'white',
+        flexDirection: 'row',
+        textAlign: 'right',
+        textAlignVertical: 'bottom',
+    },
+    backdrop: {
+        alignItems : 'stretch',
+        width : '100%',
+        height : 300,
+        borderWidth: 4,
+        borderColor: "#20232a",
+        borderRadius: 6,
+         
+    },
 })
